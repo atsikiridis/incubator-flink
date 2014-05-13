c4d2bf2bf115223246915dce44e41283f7428446
Artem Tsikiridis <artem.tsikiridis@cern.ch>
2014-05-13 21:31:13 +0200

    hadoopcompatibility: Implementations of basic programming interfaces and a basic driver.
    * wrappers for Mapper, Reducer and Combiner (as a local Reducer) on the new Java API
    * wrapper for OutputCollector
    * wrappers for Partitioner, values comparator.
    * a driver making it posible to run unmonitored (so far)  Hadoop jobs on Flink.
    * tests and variations of the WordCount driver exclusively in Hadoop.

---
+++ stratosphere-addons/hadoop-compatibility/src/main/java/eu/stratosphere/hadoopcompatibility/mapred/wrapper/HadoopGrouper.java
@@ -0,0 +1,89 @@
+/***********************************************************************************************************************
+ * Copyright (C) 2010-2013 by the Stratosphere project (http://stratosphere.eu)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ **********************************************************************************************************************/
+
+package eu.stratosphere.hadoopcompatibility.mapred.wrapper;
+
+import eu.stratosphere.api.java.functions.KeySelector;
+import eu.stratosphere.api.java.tuple.Tuple2;
+import eu.stratosphere.util.InstantiationUtil;
+import org.apache.hadoop.io.RawComparator;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
+import org.apache.hadoop.io.WritableUtils;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.util.ReflectionUtils;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A custom KeySelector for the grouping of values before each reduce() call.
+ */
+public class HadoopGrouper<K extends Writable,V extends Writable> extends KeySelector<Tuple2<K,V>, K> {
+
+	private RawComparator<K> comparator;
+	private JobConf jobConf;
+	private List<K> keysToCompareWith;
+	private Class<K> keyClass;
+
+	public HadoopGrouper(RawComparator<K> comparator, Class<K> keyClass) {
+		this.comparator = comparator;
+		this.jobConf = new JobConf();
+		this.keysToCompareWith = new ArrayList<K>();
+		this.keyClass = keyClass;
+	}
+
+	@Override
+	public K getKey(final Tuple2<K, V> value) {
+		final K currentKey = value.f0;
+
+		for(final K key: keysToCompareWith) {
+			final int result = comparator.compare(key, currentKey);
+			if (result == 0) {
+				return key;
+			}
+		}
+
+		keysToCompareWith.add(WritableUtils.clone(currentKey, jobConf));
+		return currentKey;
+	}
+
+	private void writeObject(ObjectOutputStream out) throws IOException {
+		jobConf.setOutputValueGroupingComparator(this.comparator.getClass());
+		jobConf.write(out);
+		out.writeObject(this.keyClass);
+		out.writeObject(this.keysToCompareWith);
+	}
+
+	@SuppressWarnings("unchecked")
+	private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+		this.jobConf = new JobConf();
+		this.jobConf.readFields(in);
+		this.keyClass = (Class<K>) in.readObject();
+		try {
+			this.comparator = this.jobConf.getOutputValueGroupingComparator();
+		}
+		catch (Exception e) {
+			if (InstantiationUtil.instantiate(this.keyClass) instanceof WritableComparable) {
+				this.comparator = WritableComparator.get(this.keyClass.asSubclass(WritableComparable.class));
+			} else {
+				throw new RuntimeException("Unable to instantiate the hadoop grouping comparator", e);
+			}
+		}
+		ReflectionUtils.setConf(this.comparator, this.jobConf);
+		this.keysToCompareWith = (List<K>) in.readObject();
+	}
+}
---
+++ stratosphere-addons/hadoop-compatibility/src/test/java/eu/stratosphere/test/hadoopcompatibility/mapred/driver/HadoopDriverCustomGroupingComparator.java
@@ -0,0 +1,59 @@
+/***********************************************************************************************************************
+ * Copyright (C) 2010-2013 by the Stratosphere project (http://stratosphere.eu)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ **********************************************************************************************************************/
+package eu.stratosphere.test.hadoopcompatibility.mapred.driver;
+
+import eu.stratosphere.hadoopcompatibility.mapred.example.HadoopWordCountVariations;
+import eu.stratosphere.test.hadoopcompatibility.HadoopTestBase;
+import eu.stratosphere.test.testdata.WordCountData;
+import org.apache.commons.lang3.StringUtils;
+
+public class HadoopDriverCustomGroupingComparator extends HadoopTestBase {
+
+	protected String textPath;
+	protected String resultPath;
+
+	private static final String RESULT = "alter 1\n" +
+			"brudersphaeren 1\n" +
+			"drei 6\n" +
+			"erzengel 2\n" +
+			"faust 1\n" +
+			"goethe 1\n" +
+			"himmel 4\n" +
+			"in 2\n" +
+			"mephistopheles 1\n" +
+			"nachher 2\n" +
+			"prolog 1\n" +
+			"raphael 1\n" +
+			"sonne 1\n" +
+			"treten 4\n" +
+			"vor 1\n" +
+			"weise 2";
+
+	@Override
+	protected void preSubmit() throws Exception {
+		textPath = createTempFile("text.txt", WordCountData.TEXT.substring(0,
+				StringUtils.ordinalIndexOf(WordCountData.TEXT, "\n", 5)));
+		resultPath = getTempDirPath("result");
+	}
+
+	@Override
+	protected void postSubmit() throws Exception {
+		compareResultsByLinesInMemory(RESULT, resultPath + "/1");
+	}
+
+	@Override
+	protected void testProgram() throws Exception {
+		HadoopWordCountVariations.WordCountCustomGroupingComparator.main(new String[]{textPath, resultPath});
+	}
+
+}
