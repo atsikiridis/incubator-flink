b05ec92479c39b82263d0600e578c9fae7e490dd
Artem Tsikiridis <atsikiri@cern.ch>
2014-07-14 23:35:32 +0200

    hadoopcompatibility: Grouping Comparator test case more correct.

---
+++ stratosphere-addons/hadoop-compatibility/src/test/java/eu/stratosphere/test/hadoopcompatibility/mapred/driver/HadoopDriverCustomGroupingComparatorITCase.java
@@ -0,0 +1,82 @@
+/***********************************************************************************************************************
+ * Copyright (C) 2010-2013 by the Stratosphere project (http://stratosphere.eu)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ **********************************************************************************************************************/
+package eu.stratosphere.test.hadoopcompatibility.mapred.driver;
+
+import eu.stratosphere.hadoopcompatibility.mapred.StratosphereHadoopJobClient;
+import eu.stratosphere.test.hadoopcompatibility.HadoopTestBase;
+import eu.stratosphere.test.testdata.WordCountData;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Assert;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class HadoopDriverCustomGroupingComparatorITCase extends HadoopTestBase {
+
+	protected String textPath;
+	protected String resultPath;
+
+	private static final String RESULT = "a 1\n" +
+			"b 1\n" +
+			"d 6\n" +
+			"e 2\n" +
+			"f 1\n" +
+			"g 1\n" +
+			"h 4\n" +
+			"i 2\n" +
+			"m 1\n" +
+			"n 2\n" +
+			"p 1\n" +
+			"r 1\n" +
+			"s 1\n" +
+			"t 4\n" +
+			"v 1\n" +
+			"w 2";
+
+	@Override
+	protected void preSubmit() throws Exception {
+		System.out.println(WordCountData.TEXT.substring(0, StringUtils.ordinalIndexOf(WordCountData.TEXT, "n", 5)));
+		textPath = createTempFile("text.txt", WordCountData.TEXT.substring(0,
+				StringUtils.ordinalIndexOf(WordCountData.TEXT, "\n", 5)));
+		resultPath = getTempDirPath("result");
+	}
+
+	@Override
+	protected void postSubmit() throws Exception {
+		compareResultsByLinesInMemory(RESULT, resultPath + "/1");
+	}
+
+	@Override
+	protected void testProgram() throws Exception {
+		HadoopWordCountVariations.WordCountCustomGroupingComparator.main(new String[]{textPath, resultPath});
+	}
+
+	public void compareResultsByLinesInMemory(String expectedResultStr, String resultPath) throws Exception {
+		ArrayList<String> list = new ArrayList<String>();
+		readAllResultLines(list, resultPath, false);
+
+		String[] result = list.toArray(new String[list.size()]);
+
+		String[] expected = expectedResultStr.isEmpty() ? new String[0] : expectedResultStr.split("\n");
+		Arrays.sort(expected);
+
+		Assert.assertEquals("Different number of lines in expected and obtained result.", expected.length, result.length);
+
+		for (int i=0; i< result.length; i++) {
+			result[i] = result[i].replace(result[i].substring(1, result[i].indexOf(" ")), "");
+		}
+		Assert.assertArrayEquals(expected, result);
+	}
+}
+
+
--- stratosphere-addons/hadoop-compatibility/src/test/java/eu/stratosphere/test/hadoopcompatibility/mapred/driver/HadoopDriverCustomPartitionerITCase.java
+++ stratosphere-addons/hadoop-compatibility/src/test/java/eu/stratosphere/test/hadoopcompatibility/mapred/driver/HadoopDriverCustomPartitionerITCase.java
@@ -14,45 +14,25 @@ package eu.stratosphere.test.hadoopcompatibility.mapred.driver;
 
 import eu.stratosphere.test.hadoopcompatibility.HadoopTestBase;
 import eu.stratosphere.test.testdata.WordCountData;
-import org.apache.commons.lang3.StringUtils;
 
-public class HadoopDriverCustomGroupingComparator extends HadoopTestBase {
+public class HadoopDriverCustomPartitionerITCase extends HadoopTestBase {
 
 	protected String textPath;
 	protected String resultPath;
 
-	private static final String RESULT = "alter 1\n" +
-			"brudersphaeren 1\n" +
-			"drei 6\n" +
-			"erzengel 2\n" +
-			"faust 1\n" +
-			"goethe 1\n" +
-			"himmel 4\n" +
-			"in 2\n" +
-			"mephistopheles 1\n" +
-			"nachher 2\n" +
-			"prolog 1\n" +
-			"raphael 1\n" +
-			"sonne 1\n" +
-			"treten 4\n" +
-			"vor 1\n" +
-			"weise 2";
-
 	@Override
 	protected void preSubmit() throws Exception {
-		textPath = createTempFile("text.txt", WordCountData.TEXT.substring(0,
-				StringUtils.ordinalIndexOf(WordCountData.TEXT, "\n", 5)));
+		textPath = createTempFile("text.txt", WordCountData.TEXT);
 		resultPath = getTempDirPath("result");
 	}
 
 	@Override
 	protected void postSubmit() throws Exception {
-		compareResultsByLinesInMemory(RESULT, resultPath + "/1");
+		compareResultsByLinesInMemory(WordCountData.COUNTS, resultPath + "/1");
 	}
 
 	@Override
 	protected void testProgram() throws Exception {
-		HadoopWordCountVariations.WordCountCustomGroupingComparator.main(new String[]{textPath, resultPath});
+		HadoopWordCountVariations.WordCountCustomPartitioner.main(new String[]{textPath, resultPath});
 	}
-
 }
